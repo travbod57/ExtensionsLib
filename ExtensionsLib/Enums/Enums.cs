c9c76d13bf893a23f0cebad30150950fd9e31bd8
travbod57 <alexwilliams57@hotmail.com>
2015-10-09 13:03:32 +0100

    Initial commit

---
+++ ExtensionsLib/Enums/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ExtensionsLib.Enums
+{
+    public enum ClaimsEnum
+    {
+        [Description("All Claims")]
+        AllClaims = 1,
+        [Description("Open Claims")]
+        OpenClaims = 2,
+        [Description("Closed Claims")]
+        ClosedClaims = 3
+    }
+}
---
+++ UnitTest/EnumExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ExtensionsLib.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionsLib.Extensions;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class EnumExtensionsTest
+    {
+        [TestMethod]
+        public void StringToEnum()
+        {
+            ClaimsEnum result = (ClaimsEnum)Enum.Parse(typeof(ClaimsEnum), "AllClaims");
+            Assert.IsTrue(result == ClaimsEnum.AllClaims);
+        }
+
+        [TestMethod]
+        public void StringToValue()
+        {
+            ClaimsEnum result = (ClaimsEnum)Enum.Parse(typeof(ClaimsEnum), "AllClaims");
+            Assert.IsTrue((int)result == 1);
+        }
+
+        [TestMethod]
+        public void EnumToDescription()
+        {
+            string result = ClaimsEnum.AllClaims.GetDescription();
+            Assert.IsTrue(result == "All Claims");
+        }
+    }
+}
